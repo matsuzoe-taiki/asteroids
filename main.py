08cbf3bca7cb977e52c8897e6f623c0ac4e43a35
松添大起 <matsuzoetaiki@MacBook-Air-84.local>
2026-09-20 20:36:59 +0900

    asteroidsの多様なファイルの初コミット

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    pygame.init() # pygameの各種機能を初期化（画面表示、キーボード入力、サウンド、イベント処理）
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # ゲーム画面の作成
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            pass
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+
+if __name__ == "__main__":
+    main()
