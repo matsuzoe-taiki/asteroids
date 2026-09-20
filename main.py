f669bd084b27634298b62b9a7e25e47edda2b965
松添大起 <matsuzoetaiki@MacBook-Air-84.local>
2026-09-20 21:35:31 +0900

    Draw Player

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init() # pygameの各種機能を初期化（画面表示、キーボード入力、サウンド、イベント処理）
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # ゲーム画面の作成
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0.0
 
@@ -17,18 +14,15 @@ def main():
         log_state()
 
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
 
         screen.fill("black")
-
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-
 
 if __name__ == "__main__":
     main()
