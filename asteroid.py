6d9a267988abab7384d86d6ac154fd07b3bbeba0
松添大起 <matsuzoetaiki@MacBook-Air-84.local>
2026-09-21 12:00:06 +0900

    Asteroids

---
+++ asteroid.py
@@ -0,0 +1,19 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.circle(
+            screen,
+            "White",
+            self.position,
+            self.radius
+        )
+    
+    def update(self, dt: float) -> None:
+        self.position += (self.velocity * dt)
+        
--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init() # pygameの各種機能を初期化（画面表示、キーボード入力、サウンド、イベント処理）
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # ゲーム画面の作成
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0.0
 
